182e8e860e30e839908b67113628e1482be9b2a3
Victor Ewere <vewere7@gmail.com>
2023-06-30 09:00:08 -0400

    updated ingest.sql with a fix for bike_data table

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,15 +1,17 @@
+-- RUN ./duckdb main.db -s ".read DWH-Project/sql/ingest.sql"
+
 -- drop all tables if they exist
-DROP TABLE bike_data;
-DROP TABLE central_park_weather;
-DROP TABLE daily_citi_bike_trip_counts_and_weather;
-DROP TABLE fhv_bases;
-DROP TABLE fhv_tripdata;
-DROP TABLE fhvhv_tripdata;
-DROP TABLE green_tripdata;
-DROP TABLE yellow_tripdata;
+-- DROP TABLE bike_data;
+-- DROP TABLE central_park_weather;
+-- DROP TABLE daily_citi_bike_trip_counts_and_weather;
+-- DROP TABLE fhv_bases;
+-- DROP TABLE fhv_tripdata;
+-- DROP TABLE fhvhv_tripdata;
+-- DROP TABLE green_tripdata;
+-- DROP TABLE yellow_tripdata;
 
 -- create bike_data table from csv files using glob pattern
-CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('DWH-Project/data/bike/*.csv.gz', filename=true);
+CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('DWH-Project/data/bike/*.csv.gz', filename=true, union_by_name=true, all_varchar=1);
 
 -- create central_park_weather from csv file
 CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('DWH-Project/data/central_park_weather.csv', filename=true);
