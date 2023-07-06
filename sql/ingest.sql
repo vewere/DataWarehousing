2bf5b30262380f917235689135715e980cfdb865
Victor Ewere <vewere7@gmail.com>
2023-07-05 21:01:43 -0400

    modified ingest code and outputs based on solution

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -14,22 +14,22 @@ DROP TABLE yellow_tripdata;
 CREATE TABLE bike_data AS SELECT * FROM read_csv_auto('DWH-Project/data/bike/*.csv.gz', filename=true, union_by_name=true, all_varchar=1);
 
 -- create central_park_weather from csv file
-CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('DWH-Project/data/central_park_weather.csv', filename=true);
+CREATE TABLE central_park_weather AS SELECT * FROM read_csv_auto('DWH-Project/data/central_park_weather.csv', filename=true, all_varchar=1, union_by_name=True);
 
 -- create daily_citi_bike_trip_counts_and_weather from csv file
-CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('DWH-Project/data/daily_citi_bike_trip_counts_and_weather.csv', filename=true);
+CREATE TABLE daily_citi_bike_trip_counts_and_weather AS SELECT * FROM read_csv_auto('DWH-Project/data/daily_citi_bike_trip_counts_and_weather.csv', filename=true, all_varchar=1, union_by_name=True);
 
 -- create fhv_bases from csv file
-CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('DWH-Project/data/fhv_bases.csv', filename=true);
+CREATE TABLE fhv_bases AS SELECT * FROM read_csv_auto('DWH-Project/data/fhv_bases.csv', filename=true, header=true, all_varchar=1);
 
 -- create fhv_tripdata from parquet files using glob pattern
-CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/fhv_tripdata*.parquet', filename=true);
+CREATE TABLE fhv_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/fhv_tripdata*.parquet', filename=true, union_by_name=True);
 
 -- create fhvhv_tripdata from parquet files using glob pattern
-CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/fhvhv_tripdata*.parquet', filename=true);
+CREATE TABLE fhvhv_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/fhvhv_tripdata*.parquet', filename=true, union_by_name=True);
 
 -- create green_tripdata from parquet files using glob pattern
-CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/green_tripdata*.parquet', filename=true);
+CREATE TABLE green_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/green_tripdata*.parquet', filename=true, union_by_name=True);
 
 -- create yellow_tripdata from parquet files using glob pattern
-CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/yellow_tripdata*.parquet', filename=true);
+CREATE TABLE yellow_tripdata AS SELECT * FROM read_parquet('DWH-Project/data/taxi/yellow_tripdata*.parquet', filename=true, union_by_name=True);
