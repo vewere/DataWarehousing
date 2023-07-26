449c2a057a0ff8a56a4cd13f7772740e0498f104
Victor Ewere <vewere7@gmail.com>
2023-07-26 15:54:22 -0400

    added correct interborough code and result

--- nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+++ nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
@@ -1,18 +1,43 @@
 -- for each weekday, get the number of interborough trips and % of interborough trips
 -- trip pickup date is used to determine the weekday of the trip
-select dayname(pickup_datetime) as weekday,
-       sum(case 
-             when start_loc.borough != end_loc.borough then 1 else 0
-           end) interborough_trips_count, -- count the cases where start and end boroughs are different
-       (sum(case 
-             when start_loc.borough != end_loc.borough then 1 else 0
-           end) / count(pickup_datetime) * 100) as "% of Interborough trips", -- calculate % of interborough trips
-  from {{ ref('mart__fact_all_taxi_trips') }} 
-  left join {{ ref('mart__dim_locations') }} start_loc on (pulocationid = locationid) -- left join ensures that no trip record is lost while joining
-  left join {{ ref('mart__dim_locations') }} end_loc on (dolocationid = end_loc.locationid)
- where pickup_datetime not null -- ensure that pickup/dropoff dates are not null
-   and dropOff_datetime not null
- group by all
+-- select dayname(pickup_datetime) as weekday,
+--        sum(case 
+--              when start_loc.borough != end_loc.borough then 1 else 0
+--            end) interborough_trips_count, -- count the cases where start and end boroughs are different
+--        (sum(case 
+--              when start_loc.borough != end_loc.borough then 1 else 0
+--            end) / count(pickup_datetime) * 100) as "% of Interborough trips", -- calculate % of interborough trips
+--   from {{ ref('mart__fact_all_taxi_trips') }} 
+--   left join {{ ref('mart__dim_locations') }} start_loc on (pulocationid = locationid) -- left join ensures that no trip record is lost while joining
+--   left join {{ ref('mart__dim_locations') }} end_loc on (dolocationid = end_loc.locationid)
+--  where pickup_datetime not null -- ensure that pickup/dropoff dates are not null
+--    and dropOff_datetime not null
+--  group by all
+
+with all_trips as
+(select 
+    weekday(pickup_datetime) as weekday, 
+    count(*) trips
+    from {{ ref('mart__fact_all_taxi_trips') }} t
+    where PUlocationID is not null and DOlocationID is not null
+    group by all),
+
+inter_borough as
+(select 
+    weekday(pickup_datetime) as weekday, 
+    count(*) as trips
+from {{ ref('mart__fact_all_taxi_trips') }} t
+join {{ ref('mart__dim_locations') }} pl on t.PUlocationID = pl.LocationID
+join {{ ref('mart__dim_locations') }} dl on t.DOlocationID = dl.LocationID
+where pl.borough != dl.borough
+group by all)
+
+select all_trips.weekday,
+       all_trips.trips as all_trips,
+       inter_borough.trips as inter_borough_trips,
+       inter_borough.trips / all_trips.trips as percent_inter_borough
+from all_trips
+join inter_borough on (all_trips.weekday = inter_borough.weekday);
 
 
 -- -- another method I tried that eventually gave similar results
